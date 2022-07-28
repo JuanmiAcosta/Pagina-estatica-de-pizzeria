826bf49889fb217fa76fcae2a95850e13b242772
Juan Miguel Acosta Ortega <100489090+JuanmiAcosta@users.noreply.github.com>
2022-07-28 15:36:54 +0200

    Add files via upload

--- js/aviso-cookies.js
+++ js/aviso-cookies.js
@@ -4,11 +4,11 @@ const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
 dataLayer = [];
 
-if(!localStorage.getItem('cookies-aceptadas')){
+if (!localStorage.getItem('cookies-aceptadas')) {
 	avisoCookies.classList.add('activo');
 	fondoAvisoCookies.classList.add('activo');
 } else {
-	dataLayer.push({'event': 'cookies-aceptadas'});
+	dataLayer.push({ 'event': 'cookies-aceptadas' });
 }
 
 botonAceptarCookies.addEventListener('click', () => {
@@ -17,5 +17,5 @@ botonAceptarCookies.addEventListener('click', () => {
 
 	localStorage.setItem('cookies-aceptadas', true);
 
-	dataLayer.push({'event': 'cookies-aceptadas'});
+	dataLayer.push({ 'event': 'cookies-aceptadas' });
 });
